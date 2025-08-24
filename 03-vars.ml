dd8d15b90cb8884e489d3091a18d0130e27410ed
kishor <kishorebsc12@gmail.com>
2025-08-24 14:02:40 +0530

    added

---
+++ 03-vars.ml
@@ -0,0 +1,25 @@
+- name: Play variables
+  hosts: localhost
+  connection: local
+  vars:
+    URL: play.google.com
+  tasks:
+    - name: print URL
+      ansible.builtin.debug: 
+        msg: "{{ URL }}"
+
+- name: Task variables
+  hosts: localhost
+  connection: local
+  tasks: 
+    - name: Print URL 
+      vars: 
+        URL: task.google.com
+      ansible.built.debug:
+         msg: "{{ URL }}"
+
+- name: Role variables
+  hosts: localhost
+  connection: local
+  roles:
+    - demo
